4ee71ed802ed1398874cb29e71b9b04733b68079
amit <amit@email.com>
2023-02-02 14:25:58 +0530

    add navbar header footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from "./MComponents/Header";
+import {Todos} from "./MComponents/Todos";
+import{Footer} from "./MComponents/Footer";
 function App() {
+  let todos = [
+    {
+     sno:1,
+     titile: "Go to the market",
+     desc: "You need to go to the market to get this "
+    } ,
+    {
+      sno:1,
+      titile: "Go to the market",
+      desc: "You need to go to the market to get this "
+     },
+     {
+      sno:1,
+      titile: "Go to the market",
+      desc: "You need to go to the market to get this "
+     } ,
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title ="MyTodosList!"/>
+      <Todos todos={todos}/>
+      <Footer/>
+    </>
   );
 }
 
